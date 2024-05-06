2c7f5d5f1e05c329be3bd0d77b36f29fd1dc0726
MCarbono <marcelo.carbono@tino.com.br>
2024-05-06 01:55:06 -0300

    chore: http router

--- infra/http/router/router.go
+++ infra/http/router/router.go
@@ -2,12 +2,26 @@ package router
 
 import (
 	"net/http"
+	"payment-service-provider/infra/http/controllers"
 
 	"github.com/go-chi/chi/v5"
+	chiMiddleware "github.com/go-chi/chi/v5/middleware"
 )
 
-func NewRouter() http.Handler {
+func NewRouter(controller *controllers.TransactionController) http.Handler {
 	r := chi.NewRouter()
-
+	r.Use(chiMiddleware.Logger)
+	r.Use(chiMiddleware.StripSlashes)
+	r.Use(JSONContetTyeResponse)
+	r.Post("/transactions", controller.ProcessTransaction)
+	r.Get("/transactions/{id}", controller.ListTransactions)
+	r.Get("/balance/{id}", controller.ProcessTransaction)
 	return r
+}
+
+func JSONContetTyeResponse(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Set("Content-Type", "application/json")
+		next.ServeHTTP(w, r)
+	})
 }
