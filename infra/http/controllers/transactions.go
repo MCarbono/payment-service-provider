be2d9978e891194216ae266f59bbbe8968ee5532
MCarbono <marcelo.carbono@tino.com.br>
2024-05-09 00:21:01 -0300

    chore: controller v0

--- infra/http/controllers/transactions.go
+++ infra/http/controllers/transactions.go
@@ -4,27 +4,29 @@ import (
 	"encoding/json"
 	"net/http"
 	"payment-service-provider/application/usecase"
+
+	"github.com/go-chi/chi/v5"
 )
 
-type TransactionController struct {
+type Transaction struct {
 	processTransaction *usecase.ProcessTransaction
 	listTransactions   *usecase.ListTransactions
 	clientBalance      *usecase.ClientBalance
 }
 
-func NewTransactionController(
+func NewTransaction(
 	processTransaction *usecase.ProcessTransaction,
 	listTransactions *usecase.ListTransactions,
 	clientBalance *usecase.ClientBalance,
-) *TransactionController {
-	return &TransactionController{
+) *Transaction {
+	return &Transaction{
 		processTransaction: processTransaction,
 		listTransactions:   listTransactions,
 		clientBalance:      clientBalance,
 	}
 }
 
-func (c *TransactionController) ProcessTransaction(w http.ResponseWriter, r *http.Request) {
+func (c *Transaction) ProcessTransaction(w http.ResponseWriter, r *http.Request) {
 	var input usecase.ProcessTransactionInput
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
@@ -32,12 +34,39 @@ func (c *TransactionController) ProcessTransaction(w http.ResponseWriter, r *htt
 		json.NewEncoder(w).Encode(err.Error())
 		return
 	}
+	_, err = c.processTransaction.Execute(r.Context(), &input)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
+		return
+	}
+	w.WriteHeader(http.StatusCreated)
 }
 
-func (c *TransactionController) ListTransactions(w http.ResponseWriter, r *http.Request) {
-
+func (c *Transaction) ListTransactions(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	output, err := c.listTransactions.Execute(r.Context(), &usecase.ListTransationsInput{ClientID: id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&output)
 }
 
-func (c *TransactionController) Balance(w http.ResponseWriter, r *http.Request) {
+func (c *Transaction) Balance(w http.ResponseWriter, r *http.Request) {
+	id := chi.URLParam(r, "id")
+	output, err := c.clientBalance.Execute(r.Context(), &usecase.ClientBalanceInput{ClientID: id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(&output)
+}
 
+type jsonErr struct {
+	Err string `json:"error"`
 }
