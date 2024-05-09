b2fbdd71357d16a2daa4a28de5a6399aa53793d0
MCarbono <marcelo.carbono@tino.com.br>
2024-05-09 00:22:41 -0300

    tests: refact with controller layer

--- tests/integration/clientBalance_usecase_test.go
+++ tests/integration/clientBalance_usecase_test.go
@@ -2,9 +2,15 @@ package integration
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"payment-service-provider/application/usecase"
 	"payment-service-provider/domain/entity"
 	db "payment-service-provider/infra/db/sqlc"
+	"payment-service-provider/infra/http/controllers"
+	"payment-service-provider/infra/http/router"
 	"payment-service-provider/infra/repository"
 	"testing"
 	"time"
@@ -13,94 +19,114 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldGetClientBalanceAndTheStatusOfThePayableArePaid(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
-	paymentMethod := entity.PaymentMethods["debit_card"]
-	assert.Equal(t, err, nil)
-	clientId := uuid.New().String()
-	firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
-	assert.Equal(t, err, nil)
-	secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
-	assert.Equal(t, err, nil)
-	firstPayable, err := entity.GetPayable(uuid.NewString(), firstTransaction)
-	assert.Equal(t, err, nil)
-	secondPayable, err := entity.GetPayable(uuid.NewString(), secondTransaction)
-	assert.Equal(t, err, nil)
+func TestClientBalance(t *testing.T) {
 	payableRepo := repository.NewPayableRepository(DbConn)
-	err = payableRepo.Save(context.Background(), firstPayable)
-	assert.Equal(t, err, nil)
-	err = payableRepo.Save(context.Background(), secondPayable)
-	assert.Equal(t, err, nil)
 	querier := db.New(DbConn)
 	uc := usecase.NewClientBalance(querier)
-	output, err := uc.Execute(context.Background(), &usecase.ClientBalanceInput{ClientID: clientId})
-	assert.Equal(t, err, nil)
-	assert.Equal(t, output, &usecase.ClientBalanceOutput{
-		Balance: usecase.ClientBalanceStatus{
-			Paid:      194.0,
-			WaitFunds: 0,
-		},
+	c := controllers.NewTransaction(&usecase.ProcessTransaction{}, &usecase.ListTransactions{}, uc)
+	r := router.New(c)
+	t.Run("Should get client balance that returns only payables with status 'available'", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
+		paymentMethod := entity.PaymentMethods["debit_card"]
+		assert.Equal(t, err, nil)
+		clientId := uuid.New().String()
+		firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
+		assert.Equal(t, err, nil)
+		secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
+		assert.Equal(t, err, nil)
+		firstPayable, err := entity.PayableFactory(uuid.NewString(), firstTransaction)
+		assert.Equal(t, err, nil)
+		secondPayable, err := entity.PayableFactory(uuid.NewString(), secondTransaction)
+		assert.Equal(t, err, nil)
+		err = payableRepo.Save(context.Background(), firstPayable)
+		assert.Equal(t, err, nil)
+		err = payableRepo.Save(context.Background(), secondPayable)
+		assert.Equal(t, err, nil)
+		url := fmt.Sprintf("/balance/%s", clientId)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		assert.Equal(t, err, nil)
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		var output usecase.ClientBalanceOutput
+		err = json.Unmarshal(rec.Body.Bytes(), &output)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, output, usecase.ClientBalanceOutput{
+			Balance: usecase.ClientBalanceStatus{
+				Paid:      194.0,
+				WaitFunds: 0,
+			},
+		})
 	})
-}
 
-func TestShouldGetClientBalanceAndTheStatusOfThePayableAreWaitFunds(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
-	paymentMethod := entity.PaymentMethods["credit_card"]
-	assert.Equal(t, err, nil)
-	clientId := uuid.New().String()
-	firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
-	assert.Equal(t, err, nil)
-	secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
-	assert.Equal(t, err, nil)
-	firstPayable, err := entity.GetPayable(uuid.NewString(), firstTransaction)
-	assert.Equal(t, err, nil)
-	secondPayable, err := entity.GetPayable(uuid.NewString(), secondTransaction)
-	assert.Equal(t, err, nil)
-	payableRepo := repository.NewPayableRepository(DbConn)
-	err = payableRepo.Save(context.Background(), firstPayable)
-	assert.Equal(t, err, nil)
-	err = payableRepo.Save(context.Background(), secondPayable)
-	assert.Equal(t, err, nil)
-	querier := db.New(DbConn)
-	uc := usecase.NewClientBalance(querier)
-	output, err := uc.Execute(context.Background(), &usecase.ClientBalanceInput{ClientID: clientId})
-	assert.Equal(t, err, nil)
-	assert.Equal(t, output, &usecase.ClientBalanceOutput{
-		Balance: usecase.ClientBalanceStatus{
-			Paid:      0,
-			WaitFunds: 190,
-		},
+	t.Run("Should get client balance that returns only payables with status 'wait_funds'", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
+		paymentMethod := entity.PaymentMethods["credit_card"]
+		assert.Equal(t, err, nil)
+		clientId := uuid.New().String()
+		firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
+		assert.Equal(t, err, nil)
+		secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, paymentMethod)
+		assert.Equal(t, err, nil)
+		firstPayable, err := entity.PayableFactory(uuid.NewString(), firstTransaction)
+		assert.Equal(t, err, nil)
+		secondPayable, err := entity.PayableFactory(uuid.NewString(), secondTransaction)
+		assert.Equal(t, err, nil)
+		payableRepo := repository.NewPayableRepository(DbConn)
+		err = payableRepo.Save(context.Background(), firstPayable)
+		assert.Equal(t, err, nil)
+		err = payableRepo.Save(context.Background(), secondPayable)
+		assert.Equal(t, err, nil)
+		url := fmt.Sprintf("/balance/%s", clientId)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		assert.Equal(t, err, nil)
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		var output usecase.ClientBalanceOutput
+		err = json.Unmarshal(rec.Body.Bytes(), &output)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, output, usecase.ClientBalanceOutput{
+			Balance: usecase.ClientBalanceStatus{
+				Paid:      0,
+				WaitFunds: 190,
+			},
+		})
 	})
-}
 
-func TestShouldGetClientBalanceAndTheStatusOfThePayableAsPaidAndWaitFunds(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
-	assert.Equal(t, err, nil)
-	clientId := uuid.New().String()
-	firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 153.35, time.Now(), card, entity.PaymentMethods["credit_card"])
-	assert.Equal(t, err, nil)
-	secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 103.5, time.Now(), card, entity.PaymentMethods["debit_card"])
-	assert.Equal(t, err, nil)
-	firstPayable, err := entity.GetPayable(uuid.NewString(), firstTransaction)
-	assert.Equal(t, err, nil)
-	secondPayable, err := entity.GetPayable(uuid.NewString(), secondTransaction)
-	assert.Equal(t, err, nil)
-	payableRepo := repository.NewPayableRepository(DbConn)
-	err = payableRepo.Save(context.Background(), firstPayable)
-	assert.Equal(t, err, nil)
-	err = payableRepo.Save(context.Background(), secondPayable)
-	assert.Equal(t, err, nil)
-	querier := db.New(DbConn)
-	uc := usecase.NewClientBalance(querier)
-	output, err := uc.Execute(context.Background(), &usecase.ClientBalanceInput{ClientID: clientId})
-	assert.Equal(t, err, nil)
-	assert.Equal(t, &usecase.ClientBalanceOutput{
-		Balance: usecase.ClientBalanceStatus{
-			Paid:      100,
-			WaitFunds: 145,
-		},
-	}, output)
+	t.Run("Should get client balance that with payables with status 'available' and 'wait_funds'", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
+		assert.Equal(t, err, nil)
+		clientId := uuid.New().String()
+		firstTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 153.35, time.Now(), card, entity.PaymentMethods["credit_card"])
+		assert.Equal(t, err, nil)
+		secondTransaction, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 103.5, time.Now(), card, entity.PaymentMethods["debit_card"])
+		assert.Equal(t, err, nil)
+		firstPayable, err := entity.PayableFactory(uuid.NewString(), firstTransaction)
+		assert.Equal(t, err, nil)
+		secondPayable, err := entity.PayableFactory(uuid.NewString(), secondTransaction)
+		assert.Equal(t, err, nil)
+		payableRepo := repository.NewPayableRepository(DbConn)
+		err = payableRepo.Save(context.Background(), firstPayable)
+		assert.Equal(t, err, nil)
+		err = payableRepo.Save(context.Background(), secondPayable)
+		url := fmt.Sprintf("/balance/%s", clientId)
+		req, err := http.NewRequest(http.MethodGet, url, nil)
+		assert.Equal(t, err, nil)
+		rec := httptest.NewRecorder()
+		r.ServeHTTP(rec, req)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		var output usecase.ClientBalanceOutput
+		err = json.Unmarshal(rec.Body.Bytes(), &output)
+		assert.Equal(t, err, nil)
+		assert.Equal(t, usecase.ClientBalanceOutput{
+			Balance: usecase.ClientBalanceStatus{
+				Paid:      100,
+				WaitFunds: 145,
+			},
+		}, output)
+	})
 }
--- tests/integration/listTransactions_usecase_test.go
+++ tests/integration/listTransactions_usecase_test.go
@@ -2,9 +2,15 @@ package integration
 
 import (
 	"context"
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"net/http/httptest"
 	"payment-service-provider/application/usecase"
 	"payment-service-provider/domain/entity"
 	db "payment-service-provider/infra/db/sqlc"
+	"payment-service-provider/infra/http/controllers"
+	"payment-service-provider/infra/http/router"
 	"payment-service-provider/infra/repository"
 	"testing"
 	"time"
@@ -13,55 +19,68 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestShouldListTransactions(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
-	assert.Equal(t, err, nil)
-	clientId := uuid.New().String()
+func TestListTransactions(t *testing.T) {
 	transactionRepo := repository.NewTransactionRepository(DbConn)
-	transactionDebitCard, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, entity.PaymentMethods["debit_card"])
-	err = transactionRepo.Save(context.Background(), transactionDebitCard)
-	transactionCreditCard, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, entity.PaymentMethods["credit_card"])
-	err = transactionRepo.Save(context.Background(), transactionCreditCard)
-	assert.Equal(t, err, nil)
-	expected := []usecase.ListTransactionsOutput{
-		{
-			ID:                   transactionDebitCard.GetID(),
-			ClientID:             transactionDebitCard.GetClientID(),
-			Description:          transactionDebitCard.GetDescription(),
-			Value:                transactionDebitCard.GetValue(),
-			CardOwnerName:        transactionDebitCard.GetCard().GetOwnerName(),
-			CardVerificationCode: transactionDebitCard.GetCard().GetVerificationCode(),
-			CardLastDigits:       transactionDebitCard.GetCard().GetLastDigits(),
-			CardValidDate:        transactionDebitCard.GetCard().GetValidDate(),
-			PaymentMethod:        transactionDebitCard.GetPaymentMethod(),
-			CreatedAt:            transactionDebitCard.GetCreatedAt(),
-		},
-		{
-			ID:                   transactionCreditCard.GetID(),
-			ClientID:             transactionCreditCard.GetClientID(),
-			Description:          transactionCreditCard.GetDescription(),
-			Value:                transactionCreditCard.GetValue(),
-			CardOwnerName:        transactionCreditCard.GetCard().GetOwnerName(),
-			CardVerificationCode: transactionCreditCard.GetCard().GetVerificationCode(),
-			CardLastDigits:       transactionCreditCard.GetCard().GetLastDigits(),
-			CardValidDate:        transactionCreditCard.GetCard().GetValidDate(),
-			PaymentMethod:        transactionCreditCard.GetPaymentMethod(),
-			CreatedAt:            transactionCreditCard.GetCreatedAt(),
-		},
-	}
 	querier := db.New(DbConn)
 	uc := usecase.NewListTransactions(querier)
-	output, err := uc.Execute(context.Background(), &usecase.ListTransationsInput{ClientID: transactionDebitCard.GetClientID()})
-	assert.Equal(t, err, nil)
-	assert.Equal(t, expected, output)
-}
+	controller := controllers.NewTransaction(&usecase.ProcessTransaction{}, uc, &usecase.ClientBalance{})
+	router := router.New(controller)
+	t.Run("Should list transactions", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		card, err := entity.NewCard("Teste da Silva", "123", "1111-1111-1111-1111", time.Now().AddDate(5, 0, 0))
+		assert.Equal(t, err, nil)
+		clientId := uuid.New().String()
+		transactionDebitCard, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, entity.PaymentMethods["debit_card"])
+		err = transactionRepo.Save(context.Background(), transactionDebitCard)
+		transactionCreditCard, err := entity.NewTransaction(uuid.New().String(), clientId, "compra teste", 100.0, time.Now(), card, entity.PaymentMethods["credit_card"])
+		err = transactionRepo.Save(context.Background(), transactionCreditCard)
+		assert.Equal(t, err, nil)
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/transactions/%s", clientId), nil)
+		assert.Equal(t, err, nil)
+		rec := httptest.NewRecorder()
+		router.ServeHTTP(rec, req)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		var output []usecase.ListTransactionsOutput
+		err = json.Unmarshal(rec.Body.Bytes(), &output)
+		assert.Equal(t, err, nil)
+		expected := []usecase.ListTransactionsOutput{
+			{
+				ID:                   transactionDebitCard.GetID(),
+				ClientID:             transactionDebitCard.GetClientID(),
+				Description:          transactionDebitCard.GetDescription(),
+				Value:                transactionDebitCard.GetValue(),
+				CardOwnerName:        transactionDebitCard.GetCard().GetOwnerName(),
+				CardVerificationCode: transactionDebitCard.GetCard().GetVerificationCode(),
+				CardLastDigits:       transactionDebitCard.GetCard().GetLastDigits(),
+				CardValidDate:        transactionDebitCard.GetCard().GetValidDate(),
+				PaymentMethod:        transactionDebitCard.GetPaymentMethod(),
+				CreatedAt:            transactionDebitCard.GetCreatedAt(),
+			},
+			{
+				ID:                   transactionCreditCard.GetID(),
+				ClientID:             transactionCreditCard.GetClientID(),
+				Description:          transactionCreditCard.GetDescription(),
+				Value:                transactionCreditCard.GetValue(),
+				CardOwnerName:        transactionCreditCard.GetCard().GetOwnerName(),
+				CardVerificationCode: transactionCreditCard.GetCard().GetVerificationCode(),
+				CardLastDigits:       transactionCreditCard.GetCard().GetLastDigits(),
+				CardValidDate:        transactionCreditCard.GetCard().GetValidDate(),
+				PaymentMethod:        transactionCreditCard.GetPaymentMethod(),
+				CreatedAt:            transactionCreditCard.GetCreatedAt(),
+			},
+		}
+		assert.Equal(t, expected, output)
+	})
 
-func TestShouldNotListTransactions(t *testing.T) {
-	expected := []usecase.ListTransactionsOutput{}
-	querier := db.New(DbConn)
-	uc := usecase.NewListTransactions(querier)
-	output, err := uc.Execute(context.Background(), &usecase.ListTransationsInput{ClientID: uuid.NewString()})
-	assert.Equal(t, err, nil)
-	assert.Equal(t, expected, output)
+	t.Run("Should List empty transactions", func(t *testing.T) {
+		req, err := http.NewRequest(http.MethodGet, fmt.Sprintf("/transactions/%s", uuid.New().String()), nil)
+		assert.Equal(t, err, nil)
+		rec := httptest.NewRecorder()
+		router.ServeHTTP(rec, req)
+		assert.Equal(t, http.StatusOK, rec.Code)
+		var output []usecase.ListTransactionsOutput
+		err = json.Unmarshal(rec.Body.Bytes(), &output)
+		expected := []usecase.ListTransactionsOutput{}
+		assert.Equal(t, expected, output)
+	})
 }
