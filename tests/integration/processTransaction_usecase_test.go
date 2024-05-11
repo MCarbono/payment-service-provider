f705a96125883279295f4dcf4ce07827e89af120
MCarbono <marcelo.carbono@tino.com.br>
2024-05-11 18:59:09 -0300

    refact: processTransaction suite case

--- tests/integration/processTransaction_usecase_test.go
+++ tests/integration/processTransaction_usecase_test.go
@@ -11,83 +11,83 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestProcessTransactionInvalidPaymentMethod(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
+func TestProcessTransaction(t *testing.T) {
 	transactionRepo := repository.NewTransactionRepository(DbConn)
 	payableRepo := repository.NewPayableRepository(DbConn)
-	uc := usecase.NewProcessTransaction(transactionRepo, payableRepo)
-	input := &usecase.ProcessTransactionInput{
-		ClientID:      uuid.New().String(),
-		Value:         100.0,
-		Description:   "description",
-		PaymentMethod: "invalid_payment_method",
-		Card: usecase.ProcessTransactionCardInput{
-			Number:           "1111-1111-1111-1111",
-			VerificationCode: "123",
-			OwnerName:        "Teste da Silva",
-			ValidDate:        time.Now().AddDate(5, 0, 0),
-		},
-	}
-	output, err := uc.Execute(context.Background(), input)
-	assert.Nil(t, output)
-	assert.NotNil(t, err)
-	assert.Equal(t, err.Error(), "invalid payment method invalid_payment_method")
-}
+	t.Run("Should not process the transactions because the payment method is invalid", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		uc := usecase.NewProcessTransaction(Uow)
+		input := &usecase.ProcessTransactionInput{
+			ClientID:      uuid.New().String(),
+			Value:         100.0,
+			Description:   "description",
+			PaymentMethod: "invalid_payment_method",
+			Card: usecase.ProcessTransactionCardInput{
+				Number:           "1111-1111-1111-1111",
+				VerificationCode: "123",
+				OwnerName:        "Teste da Silva",
+				ValidDate:        time.Now().AddDate(5, 0, 0),
+			},
+		}
+		output, err := uc.Execute(context.Background(), input)
+		assert.Nil(t, output)
+		assert.NotNil(t, err)
+		assert.Equal(t, err.Error(), "invalid payment method invalid_payment_method")
+	})
 
-func TestShouldProcessTransactionSuccessfully(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	transactionRepo := repository.NewTransactionRepository(DbConn)
-	payableRepo := repository.NewPayableRepository(DbConn)
-	uc := usecase.NewProcessTransaction(transactionRepo, payableRepo)
-	input := &usecase.ProcessTransactionInput{
-		ClientID:      uuid.New().String(),
-		Value:         100.0,
-		Description:   "description",
-		PaymentMethod: "debit_card",
-		Card: usecase.ProcessTransactionCardInput{
-			Number:           "1111-1111-1111-1111",
-			VerificationCode: "123",
-			OwnerName:        "Teste da Silva",
-			ValidDate:        time.Now().AddDate(5, 0, 0),
-		},
-	}
-	output, err := uc.Execute(context.Background(), input)
-	assert.Nil(t, err)
-	savedTransaction, err := transactionRepo.GetByID(context.Background(), output.TransactionDTO.ID)
-	assert.Nil(t, err)
-	assert.NotNil(t, savedTransaction)
-	savedPayable, err := payableRepo.GetByID(context.Background(), output.PayableDTO.ID)
-	assert.Nil(t, err)
-	assert.NotNil(t, savedPayable)
-	savedOutput := usecase.NewProcessTransactionDTO(savedTransaction, savedPayable)
-	assert.Equal(t, savedOutput, output)
-}
+	t.Run("Should be able to process a transaction with payment method as debit card", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		uc := usecase.NewProcessTransaction(Uow)
+		input := &usecase.ProcessTransactionInput{
+			ClientID:      uuid.New().String(),
+			Value:         100.0,
+			Description:   "description",
+			PaymentMethod: "debit_card",
+			Card: usecase.ProcessTransactionCardInput{
+				Number:           "1111-1111-1111-1111",
+				VerificationCode: "123",
+				OwnerName:        "Teste da Silva",
+				ValidDate:        time.Now().AddDate(5, 0, 0),
+			},
+		}
+		output, err := uc.Execute(context.Background(), input)
+		assert.Nil(t, err)
+		savedTransaction, err := transactionRepo.GetByID(context.Background(), output.TransactionDTO.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, savedTransaction)
+		savedPayable, err := payableRepo.GetByID(context.Background(), output.PayableDTO.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, savedPayable)
+		savedOutput := usecase.NewProcessTransactionDTO(savedTransaction, savedPayable)
+		assert.Equal(t, savedOutput, output)
+	})
 
-func TestShouldProcessTransactionSuccessfullyCreditCard(t *testing.T) {
-	defer cleanDatabaseTables(DbConn)
-	transactionRepo := repository.NewTransactionRepository(DbConn)
-	payableRepo := repository.NewPayableRepository(DbConn)
-	uc := usecase.NewProcessTransaction(transactionRepo, payableRepo)
-	input := &usecase.ProcessTransactionInput{
-		ClientID:      uuid.New().String(),
-		Value:         100.0,
-		Description:   "description",
-		PaymentMethod: "credit_card",
-		Card: usecase.ProcessTransactionCardInput{
-			Number:           "1111-1111-1111-1111",
-			VerificationCode: "123",
-			OwnerName:        "Teste da Silva",
-			ValidDate:        time.Now().AddDate(5, 0, 0),
-		},
-	}
-	output, err := uc.Execute(context.Background(), input)
-	assert.Nil(t, err)
-	savedTransaction, err := transactionRepo.GetByID(context.Background(), output.TransactionDTO.ID)
-	assert.Nil(t, err)
-	assert.NotNil(t, savedTransaction)
-	savedPayable, err := payableRepo.GetByID(context.Background(), output.PayableDTO.ID)
-	assert.Nil(t, err)
-	assert.NotNil(t, savedPayable)
-	savedOutput := usecase.NewProcessTransactionDTO(savedTransaction, savedPayable)
-	assert.Equal(t, savedOutput, output)
+	t.Run("Should be able to process a transaction with payment method as credit card", func(t *testing.T) {
+		defer cleanDatabaseTables(DbConn)
+		transactionRepo := repository.NewTransactionRepository(DbConn)
+		payableRepo := repository.NewPayableRepository(DbConn)
+		uc := usecase.NewProcessTransaction(Uow)
+		input := &usecase.ProcessTransactionInput{
+			ClientID:      uuid.New().String(),
+			Value:         100.0,
+			Description:   "description",
+			PaymentMethod: "credit_card",
+			Card: usecase.ProcessTransactionCardInput{
+				Number:           "1111-1111-1111-1111",
+				VerificationCode: "123",
+				OwnerName:        "Teste da Silva",
+				ValidDate:        time.Now().AddDate(5, 0, 0),
+			},
+		}
+		output, err := uc.Execute(context.Background(), input)
+		assert.Nil(t, err)
+		savedTransaction, err := transactionRepo.GetByID(context.Background(), output.TransactionDTO.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, savedTransaction)
+		savedPayable, err := payableRepo.GetByID(context.Background(), output.PayableDTO.ID)
+		assert.Nil(t, err)
+		assert.NotNil(t, savedPayable)
+		savedOutput := usecase.NewProcessTransactionDTO(savedTransaction, savedPayable)
+		assert.Equal(t, savedOutput, output)
+	})
 }
