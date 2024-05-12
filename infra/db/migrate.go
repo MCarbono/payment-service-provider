f6d297c997698fe24cecf817b69b5f172f512f6c
MCarbono <marcelo.carbono@tino.com.br>
2024-05-12 17:11:21 -0300

    refact: migrate function into package db

---
+++ infra/db/migrate.go
@@ -0,0 +1,29 @@
+package db
+
+import (
+	"database/sql"
+	"fmt"
+
+	"github.com/golang-migrate/migrate/v4"
+	migratePg "github.com/golang-migrate/migrate/v4/database/postgres"
+	_ "github.com/golang-migrate/migrate/v4/source/file"
+)
+
+func MigrateUp(conn *sql.DB, dbName string, migrationsPath string) error {
+	driver, err := migratePg.WithInstance(conn, &migratePg.Config{
+		DatabaseName: dbName,
+	})
+	if err != nil {
+		return fmt.Errorf("failed to get DB instance: %w", err)
+	}
+	migrateClient, err := migrate.NewWithDatabaseInstance(migrationsPath, "postgres", driver)
+	if err != nil {
+		return fmt.Errorf("failed to create migrate client: %w", err)
+	}
+	err = migrateClient.Up()
+	if err != nil {
+		return fmt.Errorf("failed to perform migration: %w", err)
+	}
+	fmt.Println("Migration completed")
+	return nil
+}
