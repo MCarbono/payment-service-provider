a1fdadac486005e66baaae024d1f0c825f556f61
MCarbono <marcelo.carbono@tino.com.br>
2024-08-18 02:41:25 -0300

    refact: controller

--- infra/http/controllers/transactions.go
+++ infra/http/controllers/transactions.go
@@ -4,6 +4,7 @@ import (
 	"encoding/json"
 	"net/http"
 	"payment-service-provider/application/usecase"
+	"payment-service-provider/infra/tracing"
 
 	"github.com/go-chi/chi/v5"
 )
@@ -27,6 +28,8 @@ func NewTransaction(
 }
 
 func (c *Transaction) ProcessTransaction(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracing.Tracer.Start(r.Context(), "controller - processing transaction")
+	defer span.End()
 	var input usecase.ProcessTransactionInput
 	err := json.NewDecoder(r.Body).Decode(&input)
 	if err != nil {
@@ -35,30 +38,36 @@ func (c *Transaction) ProcessTransaction(w http.ResponseWriter, r *http.Request)
 		return
 	}
 	defer r.Body.Close()
-	_, err = c.processTransaction.Execute(r.Context(), &input)
+	_, err = c.processTransaction.Execute(ctx, &input)
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
+		json.NewEncoder(w).Encode(newControllerOutput(ctx, nil, err))
 		return
 	}
+	output := newControllerOutput(ctx, nil, nil)
 	w.WriteHeader(http.StatusCreated)
-}
-
-func (c *Transaction) ListTransactions(w http.ResponseWriter, r *http.Request) {
-	id := chi.URLParam(r, "client_id")
-	output, err := c.listTransactions.Execute(r.Context(), &usecase.ListTransationsInput{ClientID: id})
-	if err != nil {
-		w.WriteHeader(http.StatusInternalServerError)
-		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
-		return
-	}
-	w.WriteHeader(http.StatusOK)
 	json.NewEncoder(w).Encode(&output)
 }
 
-func (c *Transaction) Balance(w http.ResponseWriter, r *http.Request) {
+func (c *Transaction) ListTransactions(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracing.Tracer.Start(r.Context(), "controller - listing all transactions")
+	defer span.End()
 	id := chi.URLParam(r, "client_id")
-	output, err := c.clientBalance.Execute(r.Context(), &usecase.ClientBalanceInput{ClientID: id})
+	output, err := c.listTransactions.Execute(ctx, &usecase.ListTransationsInput{ClientID: id})
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		json.NewEncoder(w).Encode(newControllerOutput(ctx, nil, err))
+		return
+	}
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(newControllerOutput(ctx, output, err))
+}
+
+func (c *Transaction) Balance(w http.ResponseWriter, r *http.Request) {
+	ctx, span := tracing.Tracer.Start(r.Context(), "controller - client balance")
+	defer span.End()
+	id := chi.URLParam(r, "client_id")
+	output, err := c.clientBalance.Execute(ctx, &usecase.ClientBalanceInput{ClientID: id})
 	if err != nil {
 		w.WriteHeader(http.StatusInternalServerError)
 		json.NewEncoder(w).Encode(jsonErr{Err: err.Error()})
